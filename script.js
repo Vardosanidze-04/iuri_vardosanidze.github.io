a4a52331c64348e8e17d649a108eaa837333c37b
iuri vardosanidze <ivardosanidze@seu.edu.ge>
2024-12-09 18:54:15 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let myAge = 20;
+myAge = myAge*2.5;
+console.log(myAge);
+
+ let totalCourseMonths = 3;
+ let monthsPassed = 1;
+ let monthsLeft = totalCourseMonths - monthsPassed;
+ console.log(monthsLeft);
+
+ const pi = 3.14;
+ console.log(pi+50);
+ 
+
+ 
