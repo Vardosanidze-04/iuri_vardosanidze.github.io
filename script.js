7a8852a36c5860aa80935f0ee7192a54fab7c82a
iuri vardosanidze <ivardosanidze@seu.edu.ge>
2024-12-09 22:37:06 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,38 @@ console.log(myAge);
  let monthsLeft = totalCourseMonths - monthsPassed;
  console.log(monthsLeft);
 
- const pi = 3.14;
- console.log(pi+50);
+ const testBariers = 51;
+ let myResults = 48;
  
+ const testResults = myResults >= testBariers;
+ console.log(testResults);
 
- 
+ const barier = 80;
+ let javaScriptResult = 25;
+ let htmlResult = 35;
+ let cssResult = 25;
+
+ const TestResult = javaScriptResult + htmlResult + cssResult >= barier;
+ console.log(TestResult);
+
+ let president = true;
+ let premierMinister = true;
+ let generalOfArmies = false;
+
+ const bombWillLaunch = president && premierMinister && generalOfArmies;
+ console.log(bombWillLaunch);
+
+ let userLoggedIn = true;
+ let userPaid = false;
+ let userisAdmin = false;
+
+ const showProducts = userLoggedIn && (userIsAdmin || userPaid);
+ const showBuyButton = !(userIsAdmin || userPaid);
+ console.log(showProducts); 
+ console.log(showBuyButton);
+
+ let userpaid = false;
+ let userIsAdmin = true;
+
+ const showproducts = userpaid || userIsAdmin;
+ console.log(showproducts);
